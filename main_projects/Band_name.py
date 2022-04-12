508c5553c8a32c8874433ff6f82d07fff4cdacb8
veeresh soni <veereshsoni913@gmail.com>
2022-04-12 18:34:14 +0530

    Adding another section

--- main_projects/Band_name.py
+++ main_projects/Band_name.py
@@ -6,4 +6,3 @@ city = input("Please provide me your city name please\n")
 pet = input("Enter your pet name\n")
 #4. Combine the name of their city and pet and show them their band name.
 print("Your band name could be " + city + " " + pet)
-
