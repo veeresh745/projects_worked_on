e8d8f6ab4e95920a3a2fd34b31dca15071a8cb4e
veeresh soni <veereshsoni913@gmail.com>
2022-04-12 21:09:30 +0530

    Using best ronding technique

--- main_projects/tip_calculator.py
+++ main_projects/tip_calculator.py
@@ -1,11 +1,11 @@
-# If the bill was $150.00, split between 5 people, with 12% tip.
+#If the bill was $150.00, split between 5 people, with 12% tip. 
 
-# Each person should pay (150.00 / 5) * 1.12 = 33.6
-# Format the result to 2 decimal places = 33.60
+#Each person should pay (150.00 / 5) * 1.12 = 33.6
+#Format the result to 2 decimal places = 33.60
 
-# Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
+#Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
 
-# Write your code below this line 👇
+#Write your code below this line 👇
 print("Welcome to the tip calculator!")
 amount = float(input("What was the total bill?"))
 tip = int(input("How much tip would you like to give? 10, 12, or 15?"))
@@ -13,5 +13,5 @@ people = int(input("How many people to split the bill?"))
 
 bill_pay = tip / 100 * amount + amount
 final = bill_pay / people
-final = round(final, 2)
+final = "{:.2f}".format(final)
 print(f"Each person will pay {final}")
