33d69ef899d1743e0f4111e6603693fc5fe9e03d
veeresh soni <veereshsoni913@gmail.com>
2022-04-12 18:44:50 +0530

    Adding second day basics programs

---
+++ Basic_programs/life_in_weeks.py
@@ -0,0 +1,17 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+left = 90 - int(age)
+months = left * 12
+days = left * 365
+week = left * 52
+
+
+
+print(f"You have {days} days, {week} weeks, and {months} months left.")
+
+
+
