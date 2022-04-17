08cff2eab023c07e5e6b51c1a51f0950fe03c0cd
veeresh soni <veereshsoni913@gmail.com>
2022-04-17 18:23:41 +0530

    Adding some comments

--- Basic_programs/love_calculator.py
+++ Basic_programs/love_calculator.py
@@ -4,34 +4,39 @@ name1 = input("What is your name? \n")
 name2 = input("What is their name? \n")
 # 🚨 Don't change the code above 👆
 
-# Write your code below this line 👇
+#Write your code below this line 👇
 lower_name1 = name1.lower()
 lower_name2 = name2.lower()
 
+# combining both the names
 combined_string = lower_name1 + lower_name2
 
-
+#counting true
 t = combined_string.count("t")
 r = combined_string.count("r")
-u = combined_string.count("u")
+u= combined_string.count("u")
 e = combined_string.count("e")
 true = t + r + u + e
 
+#counting love
 l = combined_string.count("l")
 o = combined_string.count("o")
 v = combined_string.count("v")
 e = combined_string.count("e")
-love = l + o + v + e
+love = l + o + v +e
 
+#score concatenation as a string
 score_conc = str(true) + str(love)
+
+#score conversion as a intege for further comparison
 score = int(score_conc)
 
-
+#comparing the score and printing
 if score < 10 or score > 90:
-    print(f"Your score is {score}, you go together like coke and mentos.")
+  print(f"Your score is {score}, you go together like coke and mentos.")
 
 elif score >= 40 and score <= 50:
-    print(f"Your score is {score}, you are alright together.")
+  print(f"Your score is {score}, you are alright together.")
 
 else:
-    print(f"Your score is {score}.")
+  print(f"Your score is {score}.")
