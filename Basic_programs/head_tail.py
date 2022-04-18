5844c9bc20a39cdb25c4574e276fbdbacb23e999
veeresh soni <veereshsoni913@gmail.com>
2022-04-18 19:01:14 +0530

    Adding a random head tail generator basic one

---
+++ Basic_programs/head_tail.py
@@ -0,0 +1,15 @@
+#Remember to use the random module
+#Hint: Remember to import the random module here at the top of the file. 🎲
+import random
+	 
+# 🚨 Don't change the code below 👇
+test_seed = int(input("Create a seed number: ")) #ignore this thing, it's for online compiler to check
+random.seed(test_seed)
+ # 🚨 Don't change the code above 👆 It's only for testing your code.
+	 
+#Write the rest of your code below this line 👇
+answer = random.randint(0, 1)
+if answer == 0:
+  print("Tails")
+else:
+  print("Heads")
