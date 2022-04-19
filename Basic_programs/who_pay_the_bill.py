721d596718ac79ecab4594115af409500e7d730a
veeresh soni <veereshsoni913@gmail.com>
2022-04-19 16:50:15 +0530

    adding the program to determine who will pay the bill

---
+++ Basic_programs/who_pay_the_bill.py
@@ -0,0 +1,24 @@
+import random
+
+# 🚨 Don't change the code below 👇
+test_seed = int(input("Create a seed number: "))
+random.seed(test_seed)
+
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+# pay_bill = random.choice(names)
+
+#Another method
+#Get the total number of items in list.
+num_items = len(names)
+
+#Generate random numbers between 0 and the last index.
+random_choice = random.randint(0, num_items - 1)
+person_pay = names[random_choice]
+
+
+print(f"{person_pay} is going to buy the meal today!")
