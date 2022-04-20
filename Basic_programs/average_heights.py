3869371e1895e053392c920aaf85620dfd068f70
veeresh soni <veereshsoni913@gmail.com>
2022-04-20 23:28:40 +0530

    Adding average height program

---
+++ Basic_programs/average_heights.py
@@ -0,0 +1,17 @@
+# 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
+
+
+#Write your code below this row 👇
+sum = 0
+for i in range (n+1):
+  sum = sum + student_heights[i]
+avg = sum / (n+1)
+avg = round(avg)
+print(avg)
+
+
+
