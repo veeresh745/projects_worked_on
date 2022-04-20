952b182e8c204e8c124476a028a1735f4e5e1e8d
veeresh soni <veereshsoni913@gmail.com>
2022-04-20 09:50:55 +0530

    Adding a treasure map program to put treasure in a desired place

---
+++ Basic_programs/treasure_map.py
@@ -0,0 +1,32 @@
+# 🚨 Don't change the code below 👇
+row1 = ["⬜️","⬜️","⬜️"]
+row2 = ["⬜️","⬜️","⬜️"]
+row3 = ["⬜️","⬜️","⬜️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+#i have splitted the position to get horizontal and vertical postion
+position = int(position)
+x = position / 10
+x_int = int(x)
+y = position % 10
+
+#since index starts from 0 i have to substract 1 from x and y
+if y-1 == 0:
+  row1[x_int-1] = "X"
+elif y-1 == 1:
+  row2[x_int-1] = "X"
+else:
+  row3[x_int-1] = "X"
+
+
+
+
+
+#Write your code above this row 👆
+
+# 🚨 Don't change the code below 👇
+print(f"{row1}\n{row2}\n{row3}")
